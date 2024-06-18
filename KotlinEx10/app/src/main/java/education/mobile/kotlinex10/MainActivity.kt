b8021bf8c8ac5dbe9bd3a20c884bd6bacecc47ab
Александр Пирогов <teniks69@yandex.ru>
2024-06-18 23:53:58 +0300

    Finished tenth exercise on Kotlin

---
+++ KotlinEx10/app/src/main/java/education/mobile/kotlinex10/MainActivity.kt
@@ -0,0 +1,47 @@
+package education.mobile.kotlinex10
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import education.mobile.kotlinex10.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action1 -> {
+                binding.root.setBackgroundColor(getColor(R.color.blue_frost))
+            }
+            R.id.action2 -> {
+                binding.root.setBackgroundColor(getColor(R.color.Sandy))
+            }
+            R.id.action3 -> {
+                binding.root.setBackgroundColor(getColor(R.color.dark_green_tea))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
